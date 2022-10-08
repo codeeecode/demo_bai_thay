dfc3d42176d6a643b8e9bc127262a9389ea0709a
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-10-08 17:13:20 +0700

    ok

--- routes/student.js
+++ routes/student.js
@@ -13,4 +13,14 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/api', (req, res) => {
+  StudentModel.find((err, data) => {
+    if (!err) {
+      //res.send(data)
+      //render ra trang index ở thư mục views/student
+      res.json(data)
+    }
+  })
+})
+
 module.exports = router
